ada28468cf4626fc47b0b4fa008d18548d543038
PIPICBA <lichimedina05@gmail.com>
2026-03-06 16:33:39 -0300

    feat(direccion): crud

--- src/direccion/direccion.module.ts
+++ src/direccion/direccion.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DireccionService } from './direccion.service';
 import { DireccionController } from './direccion.controller';
+import { Direccion } from './entities/direccion.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Direccion])],
   controllers: [DireccionController],
   providers: [DireccionService],
+  exports: [DireccionService], // 💡 Tip: Expórtalo para usarlo luego en Socios
 })
 export class DireccionModule {}
--- src/direccion/direccion.service.ts
+++ src/direccion/direccion.service.ts
@@ -1,26 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateDireccionDto } from './dto/create-direccion.dto';
 import { UpdateDireccionDto } from './dto/update-direccion.dto';
+import { Direccion } from './entities/direccion.entity';
 
 @Injectable()
 export class DireccionService {
-  create(createDireccionDto: CreateDireccionDto) {
-    return 'This action adds a new direccion';
+  constructor(
+    @InjectRepository(Direccion)
+    private readonly direccionRepository: Repository<Direccion>,
+  ) {}
+
+  async create(createDireccionDto: CreateDireccionDto): Promise<Direccion> {
+    const nuevaDireccion = this.direccionRepository.create(createDireccionDto);
+    return await this.direccionRepository.save(nuevaDireccion);
   }
 
-  findAll() {
-    return `This action returns all direccion`;
+  async findAll(): Promise<Direccion[]> {
+    return await this.direccionRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} direccion`;
+  async findOne(id: number): Promise<Direccion> {
+    const direccion = await this.direccionRepository.findOneBy({ id });
+    if (!direccion) {
+      throw new NotFoundException(`Dirección con ID ${id} no encontrada`);
+    }
+    return direccion;
   }
 
-  update(id: number, updateDireccionDto: UpdateDireccionDto) {
-    return `This action updates a #${id} direccion`;
+  async update(
+    id: number,
+    updateDireccionDto: UpdateDireccionDto,
+  ): Promise<Direccion> {
+    const direccion = await this.direccionRepository.preload({
+      id: id,
+      ...updateDireccionDto,
+    });
+
+    if (!direccion) {
+      throw new NotFoundException(
+        `No se pudo actualizar: Dirección #${id} no existe`,
+      );
+    }
+
+    return await this.direccionRepository.save(direccion);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} direccion`;
+  async remove(id: number): Promise<void> {
+    const direccion = await this.findOne(id);
+    await this.direccionRepository.remove(direccion);
   }
 }
